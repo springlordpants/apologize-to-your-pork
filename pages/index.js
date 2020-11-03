e98cfe3d4feee68fedbc47368cad76b6bc54dcd6
springlordpants <springlordpants@gmail.com>
2020-11-02 16:24:55 -0800

    another set up for working

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,7 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Apologize To Your Pork</title>
-        <link rel="icon" href="#" />
-      </Head>
-
-      <main>
         <h1>Apologize To Your Pork</h1>
-      </main>
     </div>
   )
 }
