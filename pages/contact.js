e4ba1f492e006f453c717ffd6cbe5791f95af76f
springlordpants <springlordpants@gmail.com>
2020-11-05 15:08:39 -0800

    Added github and styling

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,6 @@
 import Menu from '../components/menu'
 import { Body, TitleWrapper, MenuPos, HeadTitle, PostTitle, Section, SectionBorder } from '../styles/globalStyling';
-import { LinkStyle } from '../styles/contactStyling'
+import { ContactSection, ContactContent, ContactList, ListItem } from '../styles/contactStyling';
 import GlobalFonts from '../styles/globalFonts';
 import BodyFonts from '../styles/bodyFont';
 import Container from '@material-ui/core/Container';
@@ -8,6 +8,7 @@ import TopButton from '../components/topButton';
 import Link from 'next/link';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function Contact() {
   return (
@@ -24,27 +25,46 @@ export default function Contact() {
           <TopButton />
           <SectionBorder>
             <PostTitle>Request your favorite dishes:</PostTitle>
-            <Section>
-              <PostTitle>Send your food apologies here</PostTitle>
-              <ul>
-                <li>
-                  <Link href={{
-                    pathname: 'mailto:byron.filler@gmail.com',
-                    query: {name: 'email'},
-                  }}>
-                    <a target='_blank'><MailOutlineIcon />byron.filler@gmail.com</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href={{
-                    pathname: 'https://twitter.com/filler_films',
-                    query: {name: 'twitter'},
-                  }}>
-                    <a target='_blank'><TwitterIcon />@filler_films</a>
-                  </Link>
-                </li>
-              </ul>
-            </Section>
+            <ContactSection>
+              <PostTitle>Send your food apologies to</PostTitle>
+              <ContactContent>
+                <ContactList>
+                  <ListItem>
+                    <Link href={{
+                      pathname: 'mailto:byron.filler@gmail.com',
+                      query: {name: 'email'},
+                    }}>
+                        <a target='_blank'>
+                          <MailOutlineIcon />
+                          <p>byron.filler@gmail.com</p>
+                        </a>
+                    </Link>
+                  </ListItem>
+                  <ListItem>
+                    <Link href={{
+                      pathname: 'https://twitter.com/filler_films',
+                      query: {name: 'twitter'},
+                    }}>
+                        <a target='_blank'>
+                          <TwitterIcon />
+                          <p>@filler_films</p>
+                        </a>
+                    </Link>
+                  </ListItem>
+                  <ListItem>
+                    <Link href={{
+                      pathname: 'https://github.com/springlordpants',
+                      query: {name: 'github'},
+                    }}>
+                        <a target='_blank'>
+                          <GitHubIcon />
+                          <p>@filler_films</p>
+                        </a>
+                    </Link>
+                  </ListItem>
+                </ContactList>
+              </ContactContent>
+            </ContactSection>
           </SectionBorder>
         </Container>
     </Body>
--- styles/contactStyling.js
+++ styles/contactStyling.js
@@ -1,21 +1,45 @@
 import styled from 'styled-components';
 
-const LinkStyle = styled.div`
-    a:link {
-      text-decoration: none;
-    }
+const ContactSection = styled.div`
+    padding: 25px 50px;
+    background: white;
+    border-radius: 25px;
+    font-size: 17px;
+    font-family: 'Itim';
+`;
 
-    a:visited {
-      text-decoration: none;
-    }
+const ContactContent = styled.div`
+    justify-content: center;
+`;
 
-    a:hover {
-      text-decoration: underline;
-    }
-
-    a:active {
-      text-decoration: underline;
+const ContactList = styled.ul`
+    display: grid;
+    grid-template-columns: auto auto auto;
+    padding-inline-start: 0;
+    @media(max-width:500px){
+        display: block;
     }
 `;
 
-export { LinkStyle };
+const ListItem = styled.li`
+    a:link {
+      text-decoration: none;
+      color: #69aefc;
+    }
+    a:visited {
+      text-decoration: none;
+      color: #eb3041;
+    }
+    a:hover {
+      text-decoration: underline;
+      cursor: pointer;
+    }
+    a:active {
+      text-decoration: underline;
+      color: #f6f3e0;
+    }
+    list-style-type: none;
+    text-align: center;
+`;
+
+export { ContactSection, ContactContent, ContactList, ListItem };
