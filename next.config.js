d31cc0ac6fe4a35a954bf6a971be478346f14589
springlordpants <springlordpants@gmail.com>
2020-11-10 14:13:52 -0800

    updated next.config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,13 @@
 const withImages = require ('next-images');
 module.exports = withImages();
+
+module.exports = {
+    target: 'serverless',
+    webpack: function(config) {
+        config.module.rules.push({
+            test:/\.md$/,
+            use: 'raw-loader',
+        })
+        return config
+    }
+}
