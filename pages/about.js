52783d96ebe80911c886d69d7cceb668c1d05bed
springlordpants <springlordpants@gmail.com>
2020-11-12 11:08:42 -0800

    Header metadata working

--- pages/about.js
+++ pages/about.js
@@ -1,28 +1,56 @@
-import Layout from '../components/Layout';
-import { TitleWrapper, HeadTitle, SecondTitle, PostTitle, Section, SectionBorder } from '../styles/globalStyling';
+import Layout from "../components/Layout"
+import {
+  TitleWrapper,
+  HeadTitle,
+  PostTitle,
+  Section,
+  SectionBorder,
+} from "../styles/globalStyling"
 
-const About = () => {
+const About = ({ title }) => {
   return (
-    <Layout>
+    <Layout pageTitle={`${title} | About`}>
       <TitleWrapper>
-          <HeadTitle>About</HeadTitle>
+        <HeadTitle>About</HeadTitle>
       </TitleWrapper>
       <SectionBorder>
-          <PostTitle>Based On A Purely Platonic Oral Fixation</PostTitle>
-          <Section>
-            <main>
-              <p>
-                This satirical culinary diary follows the journey of a self proclaimed "dog person", who has too many stories about living their best life through food as an experience.
-                <br /><br />
-                Inspired by Juzo Itami's cult classic film, "Tampopo", dive into a blog that covers some needless details, organically sourced from this bloggers genuinely uncultured intuition to take what you know about the cuisine you love and starfish all over it. Each recipe is tailored to instruct joy in the consumption of a meal you'll never make eye contact with again, using the same metric standards used to declare the average constitution of a tide pod challenger.
-                <br /><br />
-                You are invited to join this millennial who evokes the question we're all verbally abusing our phones about, "What the fuck do you do that you have the disposable income to travel?"
-              </p>
-            </main>
-          </Section>
+        <PostTitle>Based On A Purely Platonic Oral Fixation</PostTitle>
+        <Section>
+          <main>
+            <p>
+              This satirical culinary diary follows the journey of a self
+              proclaimed "dog person", who has too many stories about living
+              their best life through food as an experience.
+              <br />
+              <br />
+              Inspired by Juzo Itami's cult classic film, "Tampopo", dive into a
+              blog that covers some needless details, organically sourced from
+              this bloggers genuinely uncultured intuition to take what you know
+              about the cuisine you love and starfish all over it. Each recipe
+              is tailored to instruct joy in the consumption of a meal you'll
+              never make eye contact with again, using the same metric standards
+              used to declare the average constitution of a tide pod challenger.
+              <br />
+              <br />
+              You are invited to join this millennial who evokes the question
+              we're all verbally abusing our phones about, "What the fuck do you
+              do that you have the disposable income to travel?"
+            </p>
+          </main>
+        </Section>
       </SectionBorder>
     </Layout>
   )
 }
 
 export default About
+
+export async function getStaticProps() {
+  const config = await import(`../siteconfig.json`)
+
+  return {
+    props: {
+      title: config.title,
+    },
+  }
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,63 +1,90 @@
-import Layout from '../components/Layout';
-import { TitleWrapper, HeadTitle, SecondTitle, PostTitle, Section, SectionBorder } from '../styles/globalStyling';
-import { ContactSection, ContactContent, ContactList, ListItem } from '../styles/contactStyling';
-import Link from 'next/link';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import GitHubIcon from '@material-ui/icons/GitHub';
+import Layout from "../components/Layout"
+import {
+  TitleWrapper,
+  HeadTitle,
+  SecondTitle,
+  PostTitle,
+  Section,
+  SectionBorder,
+} from "../styles/globalStyling"
+import {
+  ContactSection,
+  ContactContent,
+  ContactList,
+  ListItem,
+} from "../styles/contactStyling"
+import Link from "next/link"
+import MailOutlineIcon from "@material-ui/icons/MailOutline"
+import TwitterIcon from "@material-ui/icons/Twitter"
+import GitHubIcon from "@material-ui/icons/GitHub"
 
-
-const Contact = () => {
+const Contact = ({ title }) => {
   return (
-    <Layout>
+    <Layout pageTitle={`${title} | Contact`}>
       <TitleWrapper>
-          <HeadTitle>Contact</HeadTitle>
+        <HeadTitle>Contact</HeadTitle>
       </TitleWrapper>
       <SectionBorder>
-          <PostTitle>Request your favorite dishes:</PostTitle>
-              <ContactSection>
-              <PostTitle>Send your food apologies to</PostTitle>
-              <ContactContent>
-                <ContactList>
-                  <ListItem>
-                    <Link href={{
-                      pathname: 'mailto:byron.filler@gmail.com',
-                      query: {name: 'email'},
-                    }}>
-                        <a target='_blank'>
-                          <MailOutlineIcon style={{ fontSize: 50 }} />
-                          <p>byron.filler@gmail.com</p>
-                        </a>
-                    </Link>
-                  </ListItem>
-                  <ListItem>
-                    <Link href={{
-                      pathname: 'https://twitter.com/filler_films',
-                      query: {name: 'twitter'},
-                    }}>
-                        <a target='_blank'>
-                          <TwitterIcon style={{ fontSize: 50 }} />
-                          <p>@filler_films</p>
-                        </a>
-                    </Link>
-                  </ListItem>
-                  <ListItem>
-                    <Link href={{
-                      pathname: 'https://github.com/springlordpants',
-                      query: {name: 'github'},
-                    }}>
-                        <a target='_blank'>
-                          <GitHubIcon style={{ fontSize: 50 }} />
-                          <p>@filler_films</p>
-                        </a>
-                    </Link>
-                  </ListItem>
-                </ContactList>
-              </ContactContent>
-            </ContactSection>
+        <PostTitle>Request your favorite dishes:</PostTitle>
+        <ContactSection>
+          <PostTitle>Send your food apologies to</PostTitle>
+          <ContactContent>
+            <ContactList>
+              <ListItem>
+                <Link
+                  href={{
+                    pathname: "mailto:byron.filler@gmail.com",
+                    query: { name: "email" },
+                  }}
+                >
+                  <a target="_blank">
+                    <MailOutlineIcon style={{ fontSize: 50 }} />
+                    <p>byron.filler@gmail.com</p>
+                  </a>
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  href={{
+                    pathname: "https://twitter.com/filler_films",
+                    query: { name: "twitter" },
+                  }}
+                >
+                  <a target="_blank">
+                    <TwitterIcon style={{ fontSize: 50 }} />
+                    <p>@filler_films</p>
+                  </a>
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  href={{
+                    pathname: "https://github.com/springlordpants",
+                    query: { name: "github" },
+                  }}
+                >
+                  <a target="_blank">
+                    <GitHubIcon style={{ fontSize: 50 }} />
+                    <p>@filler_films</p>
+                  </a>
+                </Link>
+              </ListItem>
+            </ContactList>
+          </ContactContent>
+        </ContactSection>
       </SectionBorder>
     </Layout>
   )
 }
 
 export default Contact
+
+export async function getStaticProps() {
+  const config = await import(`../siteconfig.json`)
+
+  return {
+    props: {
+      title: config.title,
+    },
+  }
+}
